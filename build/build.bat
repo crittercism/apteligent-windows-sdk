a29672967126dfb2b4fd01f2fc01fdfc5b64f1ac
John Bley <jbley@crittercism.com>
2013-08-27 14:52:51 -0400

    Most people spell Microsoft with a c, but not me.

--- build/build.bat
+++ build/build.bat
@@ -1,7 +1,7 @@
 setlocal
 cd ..\CrittercismWP8SDK
 rem FIXME jbley this is the default location, might want to pursue something smarter than this
-call "C:\Program Files (x86)\Mirosoft Visual Studio 11.0\VC\WPSDK\WP80\vcvarsphoneall.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\WPSDK\WP80\vcvarsphoneall.bat"
 
 msbuild /p:Configuration=Release /target:Clean
 msbuild /p:Configuration=Release
