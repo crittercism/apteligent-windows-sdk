296e590a376ab311dfd70e078df45ea7349024ee
John Bley <jbley@crittercism.com>
2013-08-28 09:44:17 -0400

    Add a public-facing README that gets opened in Visual Studio automatically when somebody installs our nuget package.

---
+++ build/README_PUBLIC.txt
@@ -0,0 +1,7 @@
+Thanks for installing Crittercism's Windows Phone 8 SDK!
+
+FIXME jbley write some basic intro stuff here in this outline
+ - sign up for a crittercism account, create an app
+ - add init() call (choose best place - probably Application_Launching())
+ - see <url> for more details
+
--- build/build.bat
+++ build/build.bat
@@ -15,6 +15,8 @@ mkdir tmp\lib\WindowsPhone8
 
 copy ..\CrittercismWP8SDK\CrittercismWP8SDK\Bin\Release\CrittercismWP8SDK.dll tmp\lib\WindowsPhone8
 copy CrittercismWP8SDK.nuspec tmp\CrittercismWP8SDK.nuspec
+rem this file will show up in Visual Studio when you install the package.
+copy README_PUBLIC.txt tmp\README.txt
 .\NuGet.exe pack tmp\CrittercismWP8SDK.nuspec
 
 
