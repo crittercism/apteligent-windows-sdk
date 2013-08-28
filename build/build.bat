df50fdfb45775c3dc9f76ced3c7d9a930043cc94
John Bley <jbley@crittercism.com>
2013-08-28 10:03:11 -0400

    Aw yeah - kicking it old school with batch file error checking and command echoing.

--- build/build.bat
+++ build/build.bat
@@ -1,10 +1,13 @@
+ECHO ON
 setlocal
 cd ..\CrittercismWP8SDK
 rem FIXME jbley this is the default location, might want to pursue something smarter than this
 call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\WPSDK\WP80\vcvarsphoneall.bat"
+ECHO ON
 
 msbuild /p:Configuration=Release /target:Clean
 msbuild /p:Configuration=Release
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 cd ..\build
 
@@ -18,6 +21,7 @@ copy CrittercismWP8SDK.nuspec tmp\CrittercismWP8SDK.nuspec
 rem this file will show up in Visual Studio when you install the package.
 copy README_PUBLIC.txt tmp\README.txt
 .\NuGet.exe pack tmp\CrittercismWP8SDK.nuspec
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 
 
