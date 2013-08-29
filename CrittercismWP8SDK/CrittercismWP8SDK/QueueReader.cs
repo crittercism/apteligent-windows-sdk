ae196865d978fbf262adb69d7c034161afbbc280
John Bley <jbley@crittercism.com>
2013-08-29 10:36:20 -0400

    Add capability to define custom endpoint URL by using an assembly attribute.

--- CrittercismWP8SDK/CrittercismWP8SDK/QueueReader.cs
+++ CrittercismWP8SDK/CrittercismWP8SDK/QueueReader.cs
@@ -12,6 +12,39 @@ namespace CrittercismSDK
 {
     class QueueReader
     {
+        internal static string HostToUse;
+
+        static QueueReader()
+        {
+            const string CRITTERCISM_API_HOST = "https://api.crittercism.com";
+
+            // Use this as follows in your test app's AssemblyInfo.cs:
+            //[assembly: AssemblyMetadata("Crittercism.CustomApiUrl", "http://127.0.0.1:8080")]
+            const string CRITTERCISM_API_HOST_OVERRIDE_KEY = "Crittercism.CustomApiUrl";
+
+            HostToUse = CRITTERCISM_API_HOST;
+            try
+            {
+                System.Reflection.Assembly asm = System.Windows.Application.Current.GetType().Assembly;
+                foreach (System.Reflection.CustomAttributeData att in asm.CustomAttributes)
+                {
+                    if (att.AttributeType.Equals(Type.GetType("System.Reflection.AssemblyMetadataAttribute")))
+                    {
+                        if (att.ConstructorArguments != null &&
+                            att.ConstructorArguments.Count == 2 &&
+                            att.ConstructorArguments[0].Value.Equals(CRITTERCISM_API_HOST_OVERRIDE_KEY))
+                        {
+                            HostToUse = att.ConstructorArguments[1].Value.ToString();
+                        }
+                    }
+                }
+            }
+            catch
+            {
+               HostToUse = CRITTERCISM_API_HOST;
+            }
+        }
+
         /// <summary>
         /// Reads the queue.
         /// </summary>
@@ -46,8 +79,6 @@ namespace CrittercismSDK
         /// <returns>   true if it succeeds, false if it fails. </returns>
         private bool SendMessage(MessageReport message)
         {
-
-            const string CRITTERCISM_API_HOST = "https://api.crittercism.com";
             // check if the communication layer is enable and if not return true.. this is used for unit testing.
             if (!Crittercism._enableCommunicationLayer)
             {
@@ -63,13 +94,13 @@ namespace CrittercismSDK
                     switch (message.GetType().Name)
                     {
                         case "AppLoad":
-                            request = (HttpWebRequest)WebRequest.Create(new Uri(CRITTERCISM_API_HOST + "/v1/loads", UriKind.Absolute));
+                            request = (HttpWebRequest)WebRequest.Create(new Uri(HostToUse + "/v1/loads", UriKind.Absolute));
                             break;
                         case "Error":
-                            request = (HttpWebRequest)WebRequest.Create(new Uri(CRITTERCISM_API_HOST + "/v1/errors", UriKind.Absolute));
+                            request = (HttpWebRequest)WebRequest.Create(new Uri(HostToUse + "/v1/errors", UriKind.Absolute));
                             break;
                         default:
-                            request = (HttpWebRequest)WebRequest.Create(new Uri(CRITTERCISM_API_HOST + "/v1/crashes", UriKind.Absolute));
+                            request = (HttpWebRequest)WebRequest.Create(new Uri(HostToUse + "/v1/crashes", UriKind.Absolute));
                             break;
                     }
 
