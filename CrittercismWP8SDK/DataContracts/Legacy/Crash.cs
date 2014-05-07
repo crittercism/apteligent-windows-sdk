f52dca340fc9de1b18e3ac0130514dfd9c68689c
David R. Albrecht <albrecht.dr@gmail.com>
2014-05-06 19:40:12 -0700

    PP-575 Unified app load WIP
    Serialization mostly works, still need to wrap the App Load object in an
    outer array

---
+++ CrittercismWP8SDK/DataContracts/Legacy/Crash.cs
@@ -0,0 +1,83 @@
+﻿// file:	DataContracts\Crash.cs
+// summary:	Implements the crash class (Unhandled Exception)
+namespace CrittercismSDK.DataContracts.Legacy
+{
+    using Microsoft.Phone.Net.NetworkInformation;
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.IO.IsolatedStorage;
+    using System.Linq;
+    using System.Runtime.Serialization;
+    using System.Text;
+    using Windows.Devices.Sensors;
+    using Windows.Graphics.Display;
+
+    /// <summary>
+    /// Crash (Unhandled Exception).
+    /// </summary>
+    [DataContract]
+    internal class Crash : MessageReport
+    {
+        /// <summary>
+        /// Gets or sets the identifier of the application.
+        /// </summary>
+        /// <value> The identifier of the application. </value>
+        [DataMember]
+        public string app_id { get; internal set; }
+
+        /// <summary>
+        /// Gets or sets the state of the application.
+        /// </summary>
+        /// <value> The application state. </value>
+        [DataMember]
+        public Dictionary<string, object> app_state { get; internal set; }
+
+        /// <summary>
+        /// Gets or sets the breadcrumbs.
+        /// </summary>
+        /// <value> The breadcrumbs. </value>
+        [DataMember]
+        public Breadcrumbs breadcrumbs { get; internal set; }
+
+        [DataMember]
+        public Dictionary<string, string> metadata { get; internal set; }
+
+        /// <summary>
+        /// Gets or sets the crash
+        /// </summary>
+        [DataMember]
+        public ExceptionObject crash { get; internal set; }
+
+        /// <summary>
+        /// Gets or sets the platform.
+        /// </summary>
+        /// <value> The platform. </value>
+        [DataMember]
+        public Platform platform { get; internal set; }
+        
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public Crash()
+        {
+        }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="appId">Identifier for the application.</param>
+        /// <param name="appVersion">The app version.</param>
+        /// <param name="currentBreadcrumbs">The current breadcrumbs.</param>
+        /// <param name="exception">The exception.</param>
+        public Crash(string appId, string appVersion, Dictionary<string,string> currentMetadata, Breadcrumbs currentBreadcrumbs, ExceptionObject exception)
+        {
+            app_id = appId;
+            app_state = ComputeAppState(appVersion);
+            metadata = currentMetadata;
+            breadcrumbs = currentBreadcrumbs;
+            crash = exception;
+            platform = new Platform();
+        }
+    }
+}
---
+++ CrittercismWP8SDK/DataContracts/Unified/AppLoad.cs
@@ -0,0 +1,70 @@
+﻿// UnifiedAppLoadMessage.cs
+// David R. Albrecht for Crittercism, Inc.
+//
+// Implementes a .NET DataContract for the new single app load format we use across all libraries.
+// Eventually we'll retire the old data format; I've started moving it aside and calling it
+// "legacy". Apologies for the code duplication, eventually we'll only have the unified
+// messages/handlers.
+//
+// There is so much wrong with this code I don't even know where to begin...we should be using
+// immutable data objects, we should use an OS-provided persistence framework rather than rolling
+// our own with files, etc...fix this as time allows
+
+namespace CrittercismSDK.DataContracts.Unified {
+
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using System.Runtime.Serialization;
+
+    [DataContract]
+    internal class AppLoadInner {
+        [DataMember]
+        public string appID;
+        [DataMember]
+        public Guid deviceID;
+        [DataMember]
+        public string crPlatform = "windows";
+        [DataMember]
+        public string crVersion = System.Reflection.Assembly.GetExecutingAssembly().
+            GetName().Version.ToString();
+        [DataMember]
+        public string deviceModel = Microsoft.Phone.Info.DeviceStatus.DeviceName;
+        [DataMember]
+        public string osName = "wp";
+        [DataMember]
+        public string osVersion = Environment.OSVersion.Version.ToString();
+        [DataMember]
+        public string carrier = Microsoft.Phone.Net.NetworkInformation.
+            DeviceNetworkInformation.CellularMobileOperator;
+        [DataMember]
+        // Note that GetExecutingAssembly wouldn't work because we (Crittercism) *are* the executing assembly
+        public string appVersion = System.Windows.Application.Current.GetType().
+            Assembly.GetName().Version.ToString();
+        [DataMember]
+        public string locale = System.Globalization.CultureInfo.CurrentCulture.Name;
+        [DataMember]
+        public Dictionary<string, object> platformSpecificData =
+            UnifiedMessageReport.GetPlatformSpecificData();
+
+        public AppLoadInner(string _appID) {
+            this.appID = _appID;
+            this.deviceID = StorageHelper.GetOrCreateDeviceId();
+        }
+    }
+    
+    [DataContract]
+    internal class AppLoad : UnifiedMessageReport {
+        [DataMember]
+        public AppLoadInner appLoads { get; internal set; }
+        [DataMember]
+        public int count = 1;
+        [DataMember]
+        public bool current = true;
+
+        public AppLoad(string _appID) {
+            this.appLoads = new AppLoadInner(_appID);
+        }
+        public AppLoad() { }
+    }
+}
