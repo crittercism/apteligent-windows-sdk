f98bbbb7e9ad69be146de49cf5a6c1f0dd672584
David R. Albrecht <albrecht.dr@gmail.com>
2014-05-12 19:06:13 -0700

    Upgrades library version to 2.1.0

--- CrittercismWP8SDK/Properties/AssemblyInfo.cs
+++ CrittercismWP8SDK/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Resources;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Crittercism, Inc.")]
 [assembly: AssemblyProduct("CrittercismWP8SDK")]
-[assembly: AssemblyCopyright("Copyright © Crittercism, Inc. 2012-2013")]
+[assembly: AssemblyCopyright("Copyright © Crittercism, Inc. 2012-2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,7 +32,7 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.1.0")]
 [assembly: AssemblyFileVersion("2.0.0.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
 
