4cb9a30972179adad93fa95a22f13f0f6e60351d
Kelly Roach <kroach@crittercism.com>
2014-08-13 15:00:14 -0700

    SPRT-65 [Windows] Phone 8 SDK crashes

---
+++ CrittercismWP8SDK/DataContracts/Platform.cs
@@ -0,0 +1,42 @@
+// Platform.cs
+// David R. Albrecht for Crittercism, Inc.
+
+namespace CrittercismSDK.DataContracts {
+    using Microsoft.Phone.Info;
+    using System;
+    using System.Runtime.Serialization;
+
+    /// <summary>
+    /// Represents the platform on which the application (app) is running.
+    /// </summary>
+    [DataContract]
+    internal class Platform {
+        /// <summary>
+        /// Identifies this library to Crittercism
+        /// </summary>
+        [DataMember]
+        public readonly string client = "wp8v2.0"; // FIXME JBLEY check before shipping, possibly determine dynamically
+
+        /// <summary>
+        /// A GUID identifying this device
+        /// </summary>
+        [DataMember]
+        public readonly string device_id;
+
+        /// <summary>
+        /// What kind of device is this? e.g. "Nokia Lumia 820"
+        /// </summary>
+        [DataMember]
+        public readonly string device_model = DeviceStatus.DeviceName;
+
+        [DataMember]
+        public readonly string os_name = "wp";
+
+        [DataMember]
+        public readonly string os_version = Environment.OSVersion.Version.ToString();
+
+        public Platform() {
+            this.device_id = StorageHelper.GetOrCreateDeviceId().ToString();
+        }
+    }
+}
