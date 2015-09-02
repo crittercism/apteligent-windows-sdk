8f750de9a95faf328c5087ce4a8c995deec132b8
Kelly Roach <kroach@crittercism.com>
2015-09-02 16:10:31 -0700

    AGNT-1122 WP8TestApp button

--- WP8TestApp/CrashSim.xaml.cs
+++ WP8TestApp/CrashSim.xaml.cs
@@ -31,6 +31,57 @@ namespace WP8TestApp {
             Crittercism.LeaveBreadcrumb(name);
         }
 
+        string[] urls=new string[] {
+            "http://www.hearst.com",
+            "http://www.urbanoutfitters.com",
+            "http://www.pinterest.com",
+            "http://www.docusign.com",
+            "http://www.netflix.com",
+            "http://www.paypal.com",
+            "http://www.groupon.com",
+            "http://www.ebay.com",
+            "http://www.yahoo.com",
+            "http://www.linkedin.com",
+            "http://www.bloomberg.com",
+            "http://www.hoteltonight.com",
+            "http://www.npr.org",
+            "http://www.samsclub.com",
+            "http://www.postmates.com",
+            "http://www.teslamotors.com",
+            "http://www.bhphotovideo.com",
+            "http://www.getkeepsafe.com",
+            "http://www.boltcreative.com",
+            "http://www.crittercism.com/customers/"
+        };
+        private void logNetworkRequestClick(object sender,RoutedEventArgs e) {
+            Random random=new Random();
+            string[] methods=new string[] { "GET","POST","HEAD","PUT" };
+            string method=methods[random.Next(0,methods.Length)];
+            string url=urls[random.Next(0,urls.Length)];
+            if (random.Next(0,2)==1) {
+                url=url+"?doYouLoveCrittercism=YES";
+            }
+            Uri uri=new Uri(url);
+            // latency in milliseconds
+            long latency=(long)Math.Floor(4000.0*random.NextDouble());
+            long bytesRead=random.Next(0,10000);
+            long bytesSent=random.Next(0,10000);
+            long responseCode=200;
+            if (random.Next(0,5)==0) {
+                // Some common response other than 200 == OK .
+                long[] responseCodes=new long[] { 301,308,400,401,402,403,404,405,408,500,502,503 };
+                responseCode=responseCodes[random.Next(0,responseCodes.Length)];
+            }
+            Crittercism.LogNetworkRequest(
+                method,
+                new Uri(url),
+                latency,
+                bytesRead,
+                bytesSent,
+                (HttpStatusCode)responseCode,
+                WebExceptionStatus.Success);
+        }
+
         private void handledExceptionClick(object sender,RoutedEventArgs e) {
             try {
                 ThrowException(); ;
@@ -46,7 +97,7 @@ namespace WP8TestApp {
             Crittercism.LogHandledException(exception);
         }
 
-        private void testCrashClick(object sender,RoutedEventArgs e)
+        private void crashClick(object sender,RoutedEventArgs e)
         {
             ThrowException();
         }
