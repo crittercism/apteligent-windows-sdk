d230fb303bec6b4bd39da800b711752d635830d6
Kelly Roach <kroach@crittercism.com>
2015-12-04 15:22:20 -0800

    AGNT-1129-breadcrumbs BreadcrumbConverter

--- CrittercismSDK/CrittercismSDK.Shared/BreadcrumbConverter.cs
+++ CrittercismSDK/CrittercismSDK.Shared/BreadcrumbConverter.cs
@@ -26,7 +26,29 @@ namespace CrittercismSDK {
                 string timestamp = DateUtils.ISO8601DateString((DateTime)((JValue)(a[(int)BreadcrumbIndex.Timestamp])).Value);
                 BreadcrumbType breadcrumbType = (BreadcrumbType)(long)((JValue)(a[(int)BreadcrumbIndex.Type])).Value;
                 Object data = null;
-                // TODO: bleah...
+                // Launch = 0,      // 0 - session launched      ; --
+                // Text,            // 1 - user breadcrumb       ; {text:,level:}
+                // Network,         // 2 - network breadcrumb    ; [verb,url,...,statusCode,errorCode]
+                // Event,           // 3 - app event             ; {event:}
+                // Reachability,    // 4 - network change        ; {change:,type:,oldType:,newType:}
+                // View,            // 5 - uiview change / load  ; {event:,viewName:}
+                // Error,           // 6 - handled exception     ; {name:,reason:}
+                // Crash,           // 7 - crash                 ; {name:,reason:}
+                if (breadcrumbType==BreadcrumbType.Launch) {
+                    // SPEC: "[Session launched] is special in that it will only have a timestamp
+                    // and breadcrumb type field".
+                } else {
+                    JToken dataToken = a[(int)BreadcrumbIndex.Data];
+                    switch (breadcrumbType) {
+                        case BreadcrumbType.Network:
+                            data = dataToken.ToObject(typeof(Endpoint));
+                            break;
+                        default:
+                            // TODO: This might be good enough, but should double check again later.
+                            data = dataToken.ToObject(typeof(Dictionary<string,Object>));
+                            break;
+                    }
+                }
                 // Call Breadcrumb constructor.
                 breadcrumb = new Breadcrumb(
                     timestamp,
